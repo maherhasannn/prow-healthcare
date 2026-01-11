e9332a66636f127f74aa80560ec3f2072e8802b0
mmmaherhasan <mmmaherhasan@gmail.com>
2026-01-12 06:34:51 +0900

    basepath update for prow-security

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: '/prow-healthcare',
-  assetPrefix: '/prow-healthcare',
+  basePath: '/prow-security',
+  assetPrefix: '/prow-security',
   images: {
     unoptimized: true,
   },
